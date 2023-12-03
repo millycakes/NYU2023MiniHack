940fa6bcbc561adff9da6b275e4f851d03e8a5e6
Nathanuel Dixon <nathan0322@hotmail.com>
2023-12-03 18:06:17 -0500

    fixed data fetching

--- client/src/components/global/Sidebar.jsx
+++ client/src/components/global/Sidebar.jsx
@@ -29,14 +29,16 @@ export default function Sidebar() {
     async function getRestaurants() {
         const res = await fetch(`${baseUrl}?query=${searchquery}`);
         const data = await res.json();
-        console.log("data:", data.filter((val) => Object.values(val).some((v) => v === null)));
+        // console.log("all data:", data);
+        // exclude where title's empty
+        console.log("data:", data.filter((val) => val.title !== null));
         // filter data that has any value of its keys as null
-        setCards(data.filter((val) => Object.values(val).some((v) => v === null)));
+        setCards(data.filter((val) => val.title !== null));
     }
 
     return (
         <div className="Frame20095 w-full sidebar" style={{ flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 10, display: 'flex'}}>
-            <div className='search flex flex-row' id='searchbar'>
+            <div className='search flex flex-row w-full' id='searchbar'>
                 <input placeholder='Search' value={searchquery} onChange={(e) => setSearchQuery(e.target.value)} className="Search" />        
                 <button onClick={() => getRestaurants()} id="searchbutton">
                     <Search />
